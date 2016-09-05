8db207e6719206d45e86f6a63b79fa5ea98c4ac9
Annila Elias E <eannila@da5-cs-bk107-08.helsinki.fi>
2016-09-05 18:48:55 +0300

    started ui

---
+++ nbproject/Makefile-Release.mk
@@ -0,0 +1,150 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_DLIB_EXT=so
+CND_CONF=Release
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/lib/common/shapes.o \
+	${OBJECTDIR}/lib/old/geometry.o \
+	${OBJECTDIR}/lib/old/predicates.o \
+	${OBJECTDIR}/lib/sweep/advancing_front.o \
+	${OBJECTDIR}/lib/sweep/cdt.o \
+	${OBJECTDIR}/lib/sweep/sweep.o \
+	${OBJECTDIR}/lib/sweep/sweep_context.o \
+	${OBJECTDIR}/src/Coords.o \
+	${OBJECTDIR}/src/Edge.o \
+	${OBJECTDIR}/src/Funnel.o \
+	${OBJECTDIR}/src/lcpc.o \
+	${OBJECTDIR}/src/lcpfinder.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lcpc
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lcpc: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lcpc ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/lib/common/shapes.o: lib/common/shapes.cc 
+	${MKDIR} -p ${OBJECTDIR}/lib/common
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/common/shapes.o lib/common/shapes.cc
+
+${OBJECTDIR}/lib/old/geometry.o: lib/old/geometry.cc 
+	${MKDIR} -p ${OBJECTDIR}/lib/old
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/old/geometry.o lib/old/geometry.cc
+
+${OBJECTDIR}/lib/old/predicates.o: lib/old/predicates.cc 
+	${MKDIR} -p ${OBJECTDIR}/lib/old
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/old/predicates.o lib/old/predicates.cc
+
+${OBJECTDIR}/lib/sweep/advancing_front.o: lib/sweep/advancing_front.cc 
+	${MKDIR} -p ${OBJECTDIR}/lib/sweep
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/sweep/advancing_front.o lib/sweep/advancing_front.cc
+
+${OBJECTDIR}/lib/sweep/cdt.o: lib/sweep/cdt.cc 
+	${MKDIR} -p ${OBJECTDIR}/lib/sweep
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/sweep/cdt.o lib/sweep/cdt.cc
+
+${OBJECTDIR}/lib/sweep/sweep.o: lib/sweep/sweep.cc 
+	${MKDIR} -p ${OBJECTDIR}/lib/sweep
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/sweep/sweep.o lib/sweep/sweep.cc
+
+${OBJECTDIR}/lib/sweep/sweep_context.o: lib/sweep/sweep_context.cc 
+	${MKDIR} -p ${OBJECTDIR}/lib/sweep
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/sweep/sweep_context.o lib/sweep/sweep_context.cc
+
+${OBJECTDIR}/src/Coords.o: src/Coords.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Coords.o src/Coords.cpp
+
+${OBJECTDIR}/src/Edge.o: src/Edge.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Edge.o src/Edge.cpp
+
+${OBJECTDIR}/src/Funnel.o: src/Funnel.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Funnel.o src/Funnel.cpp
+
+${OBJECTDIR}/src/lcpc.o: src/lcpc.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/lcpc.o src/lcpc.cpp
+
+${OBJECTDIR}/src/lcpfinder.o: src/lcpfinder.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/lcpfinder.o src/lcpfinder.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lcpc
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
--- src/lcpc.cpp
+++ src/lcpc.cpp
@@ -9,63 +9,146 @@
 #include "Funnel.h"
 #include "minHeap.h"
 #include "lcpfinder.h"
+#include "geomfunc.h"
+#include "../lib/poly2tri.h"
+#include <vector>
+#include <gdal/gdal.h>
+#include <gdal_priv.h>
+#include <gdal/ogrsf_frmts.h>
 
+std::pair<p2t::Point*,p2t::Point*> readCostSurface(char* costSurface, char* targets, char* start, LcpFinder finder) {
+    OGRDataSource *csDS;
+    csDS = OGRSFDriverRegistrar::Open(costSurface);
+    if (csDS == NULL) {
+        std::cout << "COST SURFACE NOT FOUND!\n";
+        return -1;
+    }
+    OGRDataSource *targetDS;
+    targetDS = OGRSFDriverRegistrar::Open(targets);
+    if (targetDS == NULL) {
+        std::cout << "TARGET POINTS NOT FOUND!\n";
+        return -1;
+    }
+    OGRDataSource *startDS;
+    startDS = OGRSFDriverRegistrar::Open(start);
+    if (startDS == NULL) {
+        std::cout << "START POINT NOT FOUND!\n";
+        return -1;
+    }
 
+    OGRLayer* csLr = csDS->GetLayer(0);
+    OGRLayer* targetLr = targetDS->GetLayer(0);
+    OGRLayer* startLr = startDS->GetLayer(0);
+    std::cout << csLr->GetFeatureCount() << " cost surface features found" << std::endl;
+    std::cout << targetLr->GetFeatureCount() << " target points found" << std::endl;
+    std::cout << startLr->GetFeatureCount() << " start point found" << std::endl;
+    csLr->ResetReading();
+    targetLr->ResetReading();
+    startLr->ResetReading();
+    OGRFeature *csFtre;
+    OGRFeature *targetFtre;
+    OGRFeature *startFtre = startLr->GetNextFeature();
+    OGRGeometry* startGeom = startFtre->GetGeometryRef();
+    OGRPoint* startPoint;
+    p2t::Point* startp2t;
+    std::vector<p2t::Point*> targetp2t;
+    if (startGeom != NULL
+            && wkbFlatten(startGeom->getGeometryType()) == wkbPoint) {
+        startPoint = (OGRPoint*) startGeom;
+        startp2t = new p2t::Point(startPoint->getX(), startPoint->getY());
+    } else {
+        printf("no start geometry\n");
+    }
+    int pIdx = 0;
+    while ((csFtre = csLr->GetNextFeature()) != NULL) {
+        OGRGeometry* csGeometry = csFtre->GetGeometryRef();
+        if (csGeometry != NULL
+                && wkbFlatten(csGeometry->getGeometryType()) == wkbPolygon) {
+            OGRPolygon *csPolygon = (OGRPolygon *) csGeometry;
+            OGRLinearRing* extRing = csPolygon->getExteriorRing();
+
+            std::vector<std::vector < p2t::Point*>> polygon;
+            polygon.push_back(std::vector<p2t::Point*>{});
+
+            for (unsigned int i = 0; i < extRing->getNumPoints(); i++) {
+                p2t::Point* point = new p2t::Point(extRing->getX(i), extRing->getY(i));
+                polygon[0].push_back(point);
+            }
+
+            for (unsigned int ri = 0; ri < csPolygon->getNumInteriorRings(); ri++) {
+                polygon.push_back(std::vector<p2t::Point*>{});
+                OGRLinearRing* intRing = csPolygon->getInteriorRing(ri);
+                for (unsigned int i = 0; i < intRing->getNumPoints(); i++) {
+                    p2t::Point* point = new p2t::Point(extRing->getX(i), extRing->getY(i));
+                    polygon[ri + 1].push_back(point);
+                }
+                intermidiatePoints(&polygon, 1000);
+                finder.addPolygon(polygon, 1);
+            }
+
+            if (csPolygon->IsPointOnSurface(startPoint)) {
+                finder.addSteinerPoint(startp2t, pIdx);
+            }
+            
+            // ADD TARGET POINTS TO TARGETS!
+            pIdx++;
+        } else {
+            printf("no polygon geometry\n");
+        }
+    }
+    OGRDataSource::DestroyDataSource(csDS);
+    return std::pair<p2t::Point*,std::vector<p2t::Point*>(startp2t, targetp2t);
+}
 
 int main() {
-	/*
-	 * HOW TO MAKE FAST:
-	 * A*
-	 * Data structures: Coordsin naapurimappi, Coordmappi arrayksi? Coords luokka kokonaan pois?
-	 * Minheap, parempi update operaatio
-	 *
-	 *TODO Integrate to QGIS plugin
-	 *TODO Implement A*
-	 *TODO Writing results to shapefile (in QGIS)
-	 *TODO Weakly simple polygon splitting
-	 *TODO Performance...
-	 *
-	 */
+    /*
+     * HOW TO MAKE FAST:
+     * A*
+     * Data structures: Coordsin naapurimappi, Coordmappi arrayksi? Coords luokka kokonaan pois?
+     * Minheap, parempi update operaatio
+     *
+     *TODO Integrate to QGIS plugin
+     *TODO Implement A*
+     *TODO Writing results to shapefile (in QGIS)
+     *TODO Weakly simple polygon splitting
+     *TODO Performance...
+     *
+     */
 
-	LcpFinder finder;
+    OGRRegisterAll();
+    LcpFinder finder{};
+    p2t::Point* sp = readCostSurface("testarea.shp","targets.shp","start.shp", finder);
 
-	std::vector<std::vector<p2t::Point*>> p1 = { { new p2t::Point { 0, 0 }, new p2t::Point(1, 0), new p2t::Point(1, 1), new p2t::Point(0, 1) } };
-	std::vector<std::vector<p2t::Point*>> p2 = { { new p2t::Point { 0, 1 }, new p2t::Point(1, 1), new p2t::Point(1, 2), new p2t::Point(0, 2) } };
-	std::cout << "vector created\n";
-	std::cout << p1[0].size() << std::endl;
-	intermidiatePoints(&p1, 0.5);
-	intermidiatePoints(&p2, 0.5);
-	std::cout << p1[0].size() << std::endl;
-	std::cout << "intermidiate added\n";
 
-	std::vector<p2t::Point*> stp = {new p2t::Point{0.5,0.5}};
-	finder.addPolygon(p1, 1);
-	std::cout<<"first added\n";
-	finder.addPolygon(p2, 1);
-	std::cout<<"second added\n";
 
-	finder.addSteinerPoints(stp, 0);
-	std::cout<<"Steiner added\n";
-	std::tr1::unordered_set<Coords, CoordsHasher, std::equal_to<Coords>, std::allocator<Coords> > cmap = finder.getCoordmap();
-	for(Coords c : cmap){
-		std::cout<<c.toString()<<std::endl;
-	}
+    /*
 
-	std::cout << "HALFWAY\n";
-	//finder.addPolygon(1, p2, 1, 0.5);
+    std::vector<p2t::Point*> stp = {new p2t::Point
+        {0.5, 0.5}};
 
-	std::vector<Coords> targets{Coords(1,2)};
-	targets = finder.leastCostPath(Coords(0.5, 0.5), targets);
-	std::cout << "lcp done\n";
-	for (Coords goal : targets) {
-		while (goal.getPred() != 0) {
-			std::cout << "x: " << goal.getX() << " y: " << goal.getY() << "cost: " << goal.getToStart() << std::endl;
-			goal = *goal.getPred();
-		}
-		std::cout << "x: " << goal.getX() << " y: " << goal.getY() << "cost: " << goal.getToStart() << std::endl;
 
-	}
+    finder.addSteinerPoints(stp, 0);
+    std::cout << "Steiner added\n";
+    std::tr1::unordered_set<Coords, CoordsHasher, std::equal_to<Coords>, std::allocator<Coords> > cmap = finder.getCoordmap();
+    for (Coords c : cmap) {
+        std::cout << c.toString() << std::endl;
+    }
+     */
+    std::cout << "HALFWAY\n";
+    //finder.addPolygon(1, p2, 1, 0.5);
 
-	return 0;
+    std::vector<Coords> targets{Coords(1, 2)};
+    targets = finder.leastCostPath(Coords(), targets);
+    std::cout << "lcp done\n";
+    for (Coords goal : targets) {
+        while (goal.getPred() != 0) {
+            std::cout << "x: " << goal.getX() << " y: " << goal.getY() << "cost: " << goal.getToStart() << std::endl;
+            goal = *goal.getPred();
+        }
+        std::cout << "x: " << goal.getX() << " y: " << goal.getY() << "cost: " << goal.getToStart() << std::endl;
+
+    }
+
+    return 0;
 }
 
